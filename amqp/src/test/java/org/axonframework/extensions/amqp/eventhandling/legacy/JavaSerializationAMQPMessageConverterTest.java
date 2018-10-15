6227a52da5d615257214d344e24335f6af0f1822
Steven van Beelen <steven.vanbeelen@axoniq.io>
2018-10-15 19:38:44 +0200

    Extracted axon-amqp module from axon core

---
+++ amqp/src/test/java/org/axonframework/extensions/amqp/eventhandling/legacy/JavaSerializationAMQPMessageConverterTest.java
@@ -0,0 +1,60 @@
+package org.axonframework.extensions.amqp.eventhandling.legacy;
+
+import org.axonframework.extensions.amqp.eventhandling.AMQPMessage;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.GenericEventMessage;
+import org.axonframework.eventsourcing.DomainEventMessage;
+import org.axonframework.eventsourcing.GenericDomainEventMessage;
+import org.axonframework.messaging.MetaData;
+import org.axonframework.serialization.xml.XStreamSerializer;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class JavaSerializationAMQPMessageConverterTest {
+
+    private JavaSerializationAMQPMessageConverter testSubject;
+
+    @Before
+    public void setUp() {
+        testSubject = new JavaSerializationAMQPMessageConverter(XStreamSerializer.builder().build());
+    }
+
+    @Test
+    public void testWriteAndReadEventMessage() {
+        EventMessage<?> eventMessage = GenericEventMessage.asEventMessage("SomePayload")
+                                                          .withMetaData(MetaData.with("key", "value"));
+        AMQPMessage amqpMessage = testSubject.createAMQPMessage(eventMessage);
+        EventMessage<?> actualResult = testSubject.readAMQPMessage(amqpMessage.getBody(),
+                                                                   amqpMessage.getProperties().getHeaders())
+                                                  .orElseThrow(() -> new AssertionError("Expected valid message"));
+
+        assertEquals(eventMessage.getIdentifier(), actualResult.getIdentifier());
+        assertEquals(eventMessage.getMetaData(), actualResult.getMetaData());
+        assertEquals(eventMessage.getPayload(), actualResult.getPayload());
+        assertEquals(eventMessage.getPayloadType(), actualResult.getPayloadType());
+        assertEquals(eventMessage.getTimestamp(), actualResult.getTimestamp());
+    }
+
+    @Test
+    public void testWriteAndReadDomainEventMessage() {
+        DomainEventMessage<?> eventMessage = new GenericDomainEventMessage<>(
+                "Stub", "1234", 1L, "Payload", MetaData.with("key", "value")
+        );
+        AMQPMessage amqpMessage = testSubject.createAMQPMessage(eventMessage);
+        EventMessage<?> actualResult = testSubject.readAMQPMessage(amqpMessage.getBody(),
+                                                                   amqpMessage.getProperties().getHeaders())
+                                                  .orElseThrow(() -> new AssertionError("Expected valid message"));
+
+        assertTrue(actualResult instanceof DomainEventMessage);
+        assertEquals(eventMessage.getIdentifier(), actualResult.getIdentifier());
+        assertEquals(eventMessage.getMetaData(), actualResult.getMetaData());
+        assertEquals(eventMessage.getPayload(), actualResult.getPayload());
+        assertEquals(eventMessage.getPayloadType(), actualResult.getPayloadType());
+        assertEquals(eventMessage.getTimestamp(), actualResult.getTimestamp());
+        assertEquals(
+                eventMessage.getAggregateIdentifier(), ((DomainEventMessage) actualResult).getAggregateIdentifier()
+        );
+        assertEquals(eventMessage.getSequenceNumber(), ((DomainEventMessage) actualResult).getSequenceNumber());
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+## Axon Framework
+
+A CQRS Framework for Scalable, High-Performance Java Applications
+
+For more information, visit our website: [http://www.axonframework.org](http://www.axonframework.org).
+
+To read the reference guide, visit: [https://docs.axonframework.org](https://docs.axonframework.org).
+
+## Build status and issue tracker
+
+Build status: [![Build Status](https://travis-ci.org/AxonFramework/AxonFramework.svg?branch=master)](https://travis-ci.org/AxonFramework/AxonFramework)
+
+Issues are managed in the [GitHub issue tracker](https://github.com/AxonFramework/AxonFramework/issues)
+
+If you're looking for the old issue tracker, visit [axonframework.myjetbrains.com](https://axonframework.myjetbrains.com/).
